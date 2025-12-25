f4aa7a33212d3252361bbc5b76b543c0697695c5
FallBackITA27 <diegomassanova4@gmail.com>
2025-12-25 22:46:36 +0100

    handle endianness

--- src/byte_handler.rs
+++ src/byte_handler.rs
@@ -13,63 +13,76 @@ pub(crate) union ByteHandler {
 }
 
 impl ByteHandler {
-    pub fn copy_dword(self) -> u32 {
+    pub const fn copy_dword(self) -> u32 {
         unsafe { self.dword }
     }
 
-    pub fn copy_words(self) -> [u16; 2] {
-        unsafe { self.words }
-    }
-
-    pub fn copy_word(self, idx: usize) -> u16 {
+    pub const fn copy_word(self, idx: usize) -> u16 {
         if idx > 1 {
             return 0;
         }
+
+        #[cfg(target_endian = "little")]
+        let idx = 1 - idx;
+
         unsafe { self.words[idx] }
     }
 
-    pub fn copy_bytes(self) -> [u8; 4] {
-        unsafe { self.bytes }
-    }
-
-    pub fn copy_byte(self, idx: usize) -> u8 {
+    pub const fn copy_byte(self, idx: usize) -> u8 {
         if idx > 3 {
             return 0;
         }
+
+        #[cfg(target_endian = "little")]
+        let idx = 3 - idx;
+
         unsafe { self.bytes[idx] }
     }
 
-    pub fn shift_right(&mut self, d: u8) {
+    pub const fn shift_right(&mut self, d: u8) {
         unsafe {
             self.dword >>= d;
         }
     }
 
-    pub fn shift_left(&mut self, d: u8) {
+    pub const fn shift_left(&mut self, d: u8) {
         unsafe {
             self.dword <<= d;
         }
     }
 
     /// Reads the nth bit from the right counting from 0
-    pub fn read_bool(&self, d: u8) -> bool {
+    pub const fn read_bool(&self, d: u8) -> bool {
         if d >= 32 {
             return false;
         }
-        (self.copy_dword() & (1u32 << d)) != 0
+
+        #[cfg(target_endian = "little")]
+        return (unsafe { self.dword } & (1u32 << ((d % 8) + 8 * (3 - (d / 8))))) != 0;
+
+        #[cfg(target_endian = "big")]
+        return (unsafe { self.dword } & (1u32 << d)) != 0;
     }
 }
 
 impl From<[u8; 4]> for ByteHandler {
     fn from(value: [u8; 4]) -> Self {
-        ByteHandler { bytes: value }
+        ByteHandler {
+            #[cfg(target_endian = "big")]
+            bytes: [value[0], value[1], value[2], value[3]],
+            #[cfg(target_endian = "little")]
+            bytes: [value[3], value[2], value[1], value[0]],
+        }
     }
 }
 
 impl From<[u8; 3]> for ByteHandler {
     fn from(value: [u8; 3]) -> Self {
         ByteHandler {
+            #[cfg(target_endian = "big")]
             bytes: [value[0], value[1], value[2], 0],
+            #[cfg(target_endian = "little")]
+            bytes: [0, value[2], value[1], value[0]],
         }
     }
 }
@@ -77,7 +90,10 @@ impl From<[u8; 3]> for ByteHandler {
 impl From<[u8; 2]> for ByteHandler {
     fn from(value: [u8; 2]) -> Self {
         ByteHandler {
+            #[cfg(target_endian = "big")]
             bytes: [value[0], value[1], 0, 0],
+            #[cfg(target_endian = "little")]
+            bytes: [0, 0, value[1], value[0]],
         }
     }
 }
@@ -85,7 +101,10 @@ impl From<[u8; 2]> for ByteHandler {
 impl From<u8> for ByteHandler {
     fn from(value: u8) -> Self {
         ByteHandler {
+            #[cfg(target_endian = "big")]
             bytes: [value, 0, 0, 0],
+            #[cfg(target_endian = "little")]
+            bytes: [0, 0, 0, value],
         }
     }
 }
@@ -98,13 +117,23 @@ impl From<u32> for ByteHandler {
 
 impl From<[u16; 2]> for ByteHandler {
     fn from(value: [u16; 2]) -> Self {
-        ByteHandler { words: value }
+        ByteHandler {
+            #[cfg(target_endian = "big")]
+            words: [value[0], value[1]],
+            #[cfg(target_endian = "little")]
+            words: [value[1], value[0]],
+        }
     }
 }
 
 impl From<u16> for ByteHandler {
     fn from(value: u16) -> Self {
-        ByteHandler::from([value, 0])
+        ByteHandler {
+            #[cfg(target_endian = "big")]
+            words: [value, 0],
+            #[cfg(target_endian = "little")]
+            words: [0, value],
+        }
     }
 }
 
